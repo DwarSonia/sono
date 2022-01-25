37fe4afffa93a85b957c01e7b05af18d4d91b07a
DwarSonia <dwarsonia@gmail.com>
2022-01-25 18:45:07 -0300

    opa

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
