47385fa0a5390676f37e8fd080f8a4932e5b7836
DwarSonia <98241030+DwarSonia@users.noreply.github.com>
2022-01-26 18:55:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
+pkg update
+pkg install nodejs
+pkg install libwebp
+pkg install ffmpeg
+pkg install wget
+pkg install tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm update
+cd node_modules/@adiwajshing/baileys-md
+npm i typescript -g
+tsc
+cd ../../../
